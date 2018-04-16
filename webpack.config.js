3126d9d9c0742070fff1a35f4b08cb3ab6a2777a
Jason Ehmke <jason@addshoppers.com>
2018-04-16 10:22:58 -0400

    build system

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// Is the current build a development build
+const IS_DEV = (process.env.NODE_ENV === 'dev');
+
+const dirNode = 'node_modules';
+const dirApp = path.join(__dirname, 'src');
+const dirAssets = path.join(__dirname, 'assets');
+
+const appHtmlTitle = 'Webpack Boilerplate';
+
+/**
+ * Webpack Configuration
+ */
+module.exports = {
+    entry: {
+        bundle: path.join(dirApp, 'index')
+    },
+    resolve: {
+        modules: [
+            dirNode,
+            dirApp,
+            dirAssets
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV: IS_DEV
+        }),
+
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.ejs'),
+            title: appHtmlTitle
+        }),
+        new ExtractTextPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    compact: true
+                }
+            },
+
+            // STYLES
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: IS_DEV
+                        }
+                    },
+                ]
+            },
+
+            // CSS / SASS
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development 
+                    fallback: "style-loader"
+                })
+            },
+
+            // IMAGES
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ]
+    }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpackConfig = require('./webpack.config');
+
+
+
+module.exports = merge(webpackConfig, {
+
+    devtool: 'source-map',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+    ]
+
+});

0ab6402fac6eac2794c672e54f7558322b03f66a
Jason Ehmke <jason@addshoppers.com>
2018-04-19 12:51:44 -0400

    updated to build into dist folder with versions

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpackConfig = require('./webpack.config');
-
+const pjson = require('./package.json');
 
 
 module.exports = merge(webpackConfig, {
@@ -11,12 +11,12 @@ module.exports = merge(webpackConfig, {
     devtool: 'source-map',
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist', pjson.version),
         filename: '[name].js'
     },
 
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin([`dist/${pjson.version}`]),
     ]
 
 });
